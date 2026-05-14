c9a2b4bbd2715c8f36f69d859ad071e0ab7e3322
VANESSA ANTONELLA ARAÚJO TRES <vanessa_tres@estudante.sesisenai.org.br>
2026-05-14 11:54:31 -0300

    ggg

--- src/paginas/Usuario.jsx
+++ src/paginas/Usuario.jsx
@@ -1,19 +1,39 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
+import { useState } from "react"
 
-
-export default function Usuario () {
+export default function Usuarios () {
     const [contador, setContador] = useState(0)
+    const [usuarios, setUsuarios] = useState([])
 
     useEffect(() => {
-        document.title = 'contagem' + contador 
+        document.title = 'contagem ' + contador;
+        const buscarUsuarios = async () => {
+        const resposta = await fetch('http://localhost:3000/Usuarios')
+        const data = await resposta.json()
+        setUsuarios(data)
+        console.log(data);
+        }
+        buscarUsuarios()
+
     }, [contador])
 
     return (
         <div>
-            <h1>Usuario</h1>
-            <h1>{contador}</h1> 
+            <h1>Usuarios</h1>
+            <h1>{contador}</h1>
 
-            <button onClick= {() => {setContador(contador + 1)}}>somar</button>
+            <button onClick={() => {setContador(contador + 1)}}>
+                somar
+            </button>
+
+        <ul>
+            {usuarios.map( (usuario) => (
+                <li key={usuario.id}>
+                    <b>{usuario.nome}</b>
+                    STATUS: {usuario.ativo ? 'Ativo' : 'Desativo'}
+                </li>
+            ))}
+        </ul> 
         </div>
     )
 }
